e8dce10f206a56738d312dab378e1ffe9beb9dd6
LeeviH1 <129380155+LeeviH1@users.noreply.github.com>
2023-04-26 11:17:19 +0300

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,16 @@
+<?php
+    header('Content-Type:text/html; charset=utf-8');
+
+try{
+    $palvelin = "localhost";
+    $tietokanta = "jasenet";
+    $tunnus = "root";
+    $salasana = "";
+
+    $yhteys = new PDO("mysql:host=$palvelin;dbname=$tietokanta", $tunnus, $salasana);
+    $yhteys->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch(PDOException $virhe){
+    echo "Virhe on " . $virhe->getMessage();
+}
+?>
