50ed84e0b61d4938f75d1a1038ef565cf32bfda1
alluwaa <131852126+alluwaa@users.noreply.github.com>
2023-05-11 21:21:45 +0300

    Add files via upload

--- lambs projekti/backend/UusiUutinen.php
+++ lambs projekti/backend/UusiUutinen.php
@@ -5,7 +5,7 @@
 <html>
     <head>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-        <title>Autot</title>
+        <title>Uutiset</title>
     </head>
     <body>
      <div class="container">
@@ -13,15 +13,17 @@
         <a href="index.php">Takaisin listaan</a>
         <h3>Lisää uutinen</h3>
         <table class="table-bordered">
-            <form action="lisaaUutinen.php" method="POST">
+            <form action="uutiset.php" method="POST">
                 <tr>
                     <td>Otsikko</td>
                     <td><input type="text" name="otsikko" required></td>
                 </tr>
                 <tr>
                     <td>Teksti</td>
-                    <td><input type="text" name="uutinen"></td>
+                    <td><textarea name="uutinen" style="width: 1000px; height: 500px;"></textarea></td>
+
                 </tr>
+                <tr>
                     <td></td>
                     <td><button name="talleta" type="submit" class="btn btn-primary">Talleta</button></td>
                 </tr>
