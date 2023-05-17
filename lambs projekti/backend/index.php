edaee8c71b48a95ef1731bf085689fad3bab4316
alluwaa <131852126+alluwaa@users.noreply.github.com>
2023-05-17 18:05:30 +0300

    Add files via upload

--- lambs projekti/backend/index.php
+++ lambs projekti/backend/index.php
@@ -1,38 +1,36 @@
-<?php 
-
+<?php
 require "includes/header.php";
 require "config.php";
 
 if(isset($_SESSION['kayttajatunnus'])){
     header("location: edit.php");
+    exit;
 }
 
 if(isset($_POST['submit'])){
-    if($_POST['kayttajatunnus'] == 'Admin' && $_POST['password'] == '123123'){
-        $_SESSION['kayttajatunnus'] = 'Admin';
-        header("location: edit.php");
-        exit;
-    } else {
-        $kayttajatunnus = $_POST['kayttajatunnus'];
-        $salasana = $_POST['password'];
-        $komento = "SELECT * FROM user WHERE kayttajatunnus = '$kayttajatunnus'";
-        $kirjaudu = $yhteys->prepare($komento);
-        $kirjaudu->execute();
-        $data = $kirjaudu->fetch(PDO::FETCH_ASSOC);
-        
-        if($kirjaudu->rowCount() == 1){
-            if(password_verify($salasana, $data['salasana'])){
-                $_SESSION['kayttajatunnus'] = $data['kayttajatunnus'];
-                header("location: edit.php");
-                exit;
-            } else {
-                echo "Käyttäjätunnus tai salasana on väärin";
-            }
+    $kayttajatunnus = $_POST['kayttajatunnus'];
+    $salasana = $_POST['password'];
+
+    $komento = "SELECT * FROM user WHERE kayttajatunnus = :kayttajatunnus";
+    $kirjaudu = $yhteys->prepare($komento);
+    $kirjaudu->bindValue(':kayttajatunnus', $kayttajatunnus, PDO::PARAM_STR);
+    $kirjaudu->execute();
+    $data = $kirjaudu->fetch(PDO::FETCH_ASSOC);
+    
+    if($kirjaudu->rowCount() == 1){
+        $storedPassword = $data['salasana'];
+        if($salasana == $storedPassword){
+            $_SESSION['kayttajatunnus'] = $data['kayttajatunnus'];
+            header("location: edit.php");
+            exit;
         } else {
             echo "Käyttäjätunnus tai salasana on väärin";
         }
+    } else {
+        echo "Käyttäjätunnus tai salasana on väärin";
     }
 }
+
 ?>
 
 <main class="form-signin w-50 m-auto">
