4ab448d9cdea1655d190fbc4658fc5259becb10d
jerekaartinen <129037482+jerekaartinen@users.noreply.github.com>
2023-05-18 16:58:49 +0300

    Update keikkalistaus.php
    kommentit

--- lambs projekti/backend/keikkalistaus.php
+++ lambs projekti/backend/keikkalistaus.php
@@ -1,28 +1,46 @@
 <?php
+    // Sallitaan pääsy tälle tiedostolle kaikista lähteistä
     header("Access-Control-Allow-Origin: *");
 
+    // Sisällytetään konfiguraatiotiedosto
     include('config.php');
 
-    $kysely="SELECT KeikkakalenteriID, DATE_FORMAT(paivamaara, '%d.%m.%Y') AS paivamaara, paikkakunta, paikka, TIME_FORMAT(kellonaika, '%H:%i') AS kellonaika FROM KeikkakalenteriID ORDER BY paivamaara";
-
+    // Suoritetaan tietokantakysely
+    $kysely = "SELECT KeikkakalenteriID, DATE_FORMAT(paivamaara, '%d.%m.%Y') AS paivamaara, paikkakunta, paikka, TIME_FORMAT(kellonaika, '%H:%i') AS kellonaika FROM KeikkakalenteriID ORDER BY paivamaara";
     $data = $yhteys->query($kysely);
 
+    // Alustetaan JSON-muuttuja
     $JSON = '{"keikka":[';
+
+    // Laskurimuuttuja ja rivien määrä
     $laskuri = 0;
     $riveja = $data->rowCount();
-    while($rivi = $data->fetch(PDO::FETCH_ASSOC)){
+
+    // Käydään läpi tietokantatulokset ja lisätään ne JSON-muuttujaan
+    while ($rivi = $data->fetch(PDO::FETCH_ASSOC)) {
         $laskuri++;
+        
+        // Lisätään keikan tiedot JSON-muuttujaan
         $JSON .= '{"Id":"' . $rivi['KeikkakalenteriID'] . '","Pvm":"' . $rivi['paivamaara'] . '","Klo":"' . $rivi['kellonaika'] . '","Paikkakunta":"' . $rivi['paikkakunta'] . '","Paikka":"' . $rivi['paikka'] . '"}';
 
-        if($laskuri<$riveja) $JSON.=",";
-
+        // Lisätään pilkku, jos ei ole viimeinen keikka
+        if ($laskuri < $riveja) {
+            $JSON .= ",";
+        }
     }
 
-        $JSON.=']}';
-        $yhteys = null;
+    // Viimeistellään JSON-muuttuja
+    $JSON .= ']}';
 
-        $handler = fopen("keikat.json", "w");
-        fwrite($handler, $JSON);
-        fclose($handler);
+    // Suljetaan tietokantayhteys
+    $yhteys = null;
 
+    // Avataan tiedosto "keikat.json" kirjoitusta varten
+    $handler = fopen("keikat.json", "w"); 
+
+    // Kirjoitetaan JSON-muuttuja tiedostoon
+    fwrite($handler, $JSON);
+
+    // Suljetaan tiedosto
+    fclose($handler);
 ?>
